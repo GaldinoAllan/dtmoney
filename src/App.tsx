0809ebf111c8f4f9122dea5e66ae3ff829adab98
allan <galdinoallancodes@gmail.com>
2021-03-18 17:51:16 -0300

    everything configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import './styles/global.css'
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
+      <GlobalStyle />
+
       <h1>Hello World</h1>
     </div>
   );
