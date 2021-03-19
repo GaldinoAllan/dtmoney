4a3960e27dff23e7de15eb4f15e3cec81ec631c7
allan <galdinoallancodes@gmail.com>
2021-03-19 18:35:50 -0300

    adding modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
+import Modal from 'react-modal'
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 
 import { GlobalStyle } from "./styles/global";
+import { useState } from 'react';
+
+// const customStyles = {
+//   content: {
+//     top: '50%',
+//     left: '50%',
+//     right: 'auto',
+//     bottom: 'auto',
+//     marginRight: '-50%',
+//     transform: 'translate(-50%, -50%)'
+//   }
+// };
 
 export function App() {
+  const [
+    isNewTransactionModalOpen,
+    setIsNewTransactionModalOpen
+  ] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <GlobalStyle />
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      // style={customStyles}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+      <GlobalStyle />
     </>
   );
 }
