0c038d5c8947f1eab8a88b18f68ba5475746763f
allan <galdinoallancodes@gmail.com>
2021-03-23 21:13:36 -0300

    getting from api mock successfully

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import Modal from 'react-modal'
 
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+import { api } from '../../services/api'
 
 import {
   Container,
@@ -23,7 +24,23 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose
 }: NewTransactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
   const [type, setType] = useState('deposit');
+  const [category, setCategory] = useState('');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      title,
+      value,
+      type,
+      category
+    }
+
+    api.post('/transactions', data)
+  }
 
   return (
     <Modal
@@ -39,11 +56,20 @@ export function NewTransactionModal({
       >
         <img src={closeImg} alt="fechar modal" />
       </button>
-      <Container>
+      <Container onSubmit={event => handleCreateNewTransaction(event)}>
         <h2>Cadastrar transação</h2>
 
-        <input placeholder="Título" />
-        <input type="number" placeholder="Valor" />
+        <input
+          placeholder="Título"
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Valor"
+          value={value}
+          onChange={event => setValue(Number(event.target.value))}
+        />
 
         <TransactionTypeContainer>
           <RadioBox
@@ -67,7 +93,11 @@ export function NewTransactionModal({
           </RadioBox>
         </TransactionTypeContainer>
 
-        <input placeholder="Categoria" />
+        <input
+          placeholder="Categoria"
+          value={category}
+          onChange={event => setCategory(event.target.value)}
+        />
 
         <button type="submit">
           Cadastrar
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -10,49 +10,48 @@ interface Transaction {
   amount: number;
   type: string;
   category: string;
-  createdAt: Date;
+  createdAt: string;
 }
 
 export function TransactionsTable() {
   const [transactions, setTransactions] = useState<Transaction[]>([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     api.get('transactions')
-      .then(response => {
-        setTransactions(response.data)
-      })
+      .then(response => setTransactions(response.data.transactions))
+
+    setLoading(false)
   }, [])
 
   return (
     <Container>
-      <table>
-        <thead>
-          <tr>
-            <th>Título</th>
-            <th>Valor</th>
-            <th>Categoria</th>
-            <th>Data</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            transactions.map(transaction => (
-              <tr key={transaction.id}>
-                <td>{transaction.title}</td>
-                <td className="deposit">${transaction.amount}</td>
-                <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+      {
+        loading
+          ? <span>Loading...</span>
+          : <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Valor</th>
+                <th>Categoria</th>
+                <th>Data</th>
               </tr>
-            ))
-          }
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">- $1.100,00</td>
-            <td>Casa</td>
-            <td>17/02/21</td>
-          </tr>
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {
+                transactions.map(transaction => (
+                  <tr key={transaction.id}>
+                    <td>{transaction.title}</td>
+                    <td className={transaction.type}>${transaction.amount}</td>
+                    <td>{transaction.category}</td>
+                    <td>{transaction.createdAt}</td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+      }
     </Container>
   )
 }
